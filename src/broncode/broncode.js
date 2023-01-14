d0f4d50376633ba3a7702ab2f333322137fa3a73
Ellen van Duikeren <e.vanduikeren@gmail.com>
2023-01-14 13:12:59 +0100

    Made searchbar in recipes

---
+++ src/broncode/broncode.js
@@ -0,0 +1,207 @@
+// recipes searchbar
+
+import {Link} from "react-router-dom";
+import React from "react";
+
+{/*{*/}
+{/*    recipes.filter(recipe => {*/}
+{/*        if (query === "") {*/}
+{/*            return recipe;*/}
+{/*        } else if (recipe.title.toLowerCase().includes(query.toLowerCase())) {*/}
+{/*            return recipe;*/}
+{/*        }*/}
+{/*    }).map((recipe) => (*/}
+{/*        <p>{recipe.title}</p>*/}
+{/*    ))*/}
+{/*}*/}
+
+
+{/*{*/}
+{/*    recipes.filter(recipe => {*/}
+{/*        if (query === "") {*/}
+{/*            return recipe;*/}
+{/*        } else {*/}
+{/*            for (let i = 0; i < recipe.months.length; i++) {*/}
+{/*                if (recipe.months[i].toLowerCase().includes(query.toLowerCase())*/}
+{/*                    || (recipe.months[i].toLowerCase().includes("jaarrond"))) {*/}
+{/*                    return recipe;*/}
+{/*                }*/}
+{/*            }*/}
+{/*        }*/}
+{/*    }).map((recipe, index) => (*/}
+{/*        <div key={index} className="recipes__div--flex">*/}
+{/*            <p>{recipe.title}</p>*/}
+{/*        </div>*/}
+{/*    ))*/}
+{/*}*/}
+
+
+{/*{*/}
+{/*    recipes.filter(recipe => {*/}
+{/*        if (query === "") {*/}
+{/*            return recipe;*/}
+{/*        } else {*/}
+{/*            for (let i = 0; i < recipe.tags.length; i++) {*/}
+{/*                if (recipe.tags[i].toLowerCase().includes(query.toLowerCase())) {*/}
+{/*                    return recipe;*/}
+{/*                }*/}
+{/*            }*/}
+{/*        }*/}
+{/*    }).map((recipe) => (*/}
+{/*        <p key={recipe.id}>{recipe.title}</p>*/}
+{/*    ))*/}
+{/*}*/}
+
+
+{/*{*/}
+{/*    recipes.filter(recipe => {*/}
+{/*        if (query === "") {*/}
+{/*            return recipe;*/}
+{/*        } else {*/}
+{/*            for (let i = 0; i < recipe.ingredients.length; i++) {*/}
+{/*                if (recipe.ingredients[i].ingredient_name.toLowerCase().includes(query.toLowerCase())) {*/}
+{/*                    return recipe;*/}
+{/*                }*/}
+{/*            }*/}
+{/*        }*/}
+{/*    }).map((recipe) => (*/}
+{/*        <p key={recipe.id}>{recipe.title}</p>*/}
+{/*    ))*/}
+{/*}*/}
+
+// <ul className="recipes__ul">
+//     <div className="recipes__div">
+//         {recipes.map((recipe) => {
+//             return <li key={recipe.id}
+//                        className="recipes__li">
+//                 <Link
+//                     to={"/recipe/" + recipe.id}
+//                     className="recipes__a"
+//                 >
+//                     {recipe.file &&
+//                         <img
+//                             src={recipe.file.url}
+//                             alt={recipe.name}
+//                             className="recipes__image"
+//                         />}
+//                     {recipe.title}
+//                 </Link>
+//             </li>
+//         })}
+//     </div>
+// </ul>
+//
+
+
+// <article className="recipes__article--flex">
+//     { ((query === "") && (month.includes("selecteer") && tag.includes("selecteer")) &&
+//         recipes.filter(recipe => {
+//             return recipe;
+//             //if query is not empty alias else
+//         } else if (recipe.title.toLowerCase().includes(query.toLowerCase())) {
+//         return recipe;
+//     } else {
+//         //search for ingredients
+//         for (let i = 0; i < recipe.ingredients.length; i++) {
+//         if (recipe.ingredients[i].ingredient_name.toLowerCase().includes(query.toLowerCase())) {
+//         return recipe;
+//     }
+//     }
+//     }
+//     }).map((recipe) => (
+//     <ul className="recipes__ul">
+//         <div>
+//             <li key={recipe.id}
+//                 className="recipes__li">
+//                 <Link
+//                     to={"/recipe/" + recipe.id}
+//                     className="recipes__a"
+//                 >
+//                     {recipe.file &&
+//                         <img
+//                             src={recipe.file.url}
+//                             alt={recipe.name}
+//                             className="recipes__image"
+//                         />}
+//                     {recipe.title}
+//                 </Link>
+//             </li>
+//         </div>
+//     </ul>
+//     ))
+//     }
+// </article>
+//
+//
+//
+// {!month.includes("selecteer") && <h3 className="recipes__h3">Geselecteerd op de maand {month}, inclusief jaarrond</h3>}
+// <article className="recipes__article--flex">
+//     {!month.includes("selecteer") && (
+//         recipes.filter(recipe => {
+//             for (let i = 0; i < recipe.months.length; i++) {
+//                 if (recipe.months[i].toLowerCase().includes(month.toLowerCase())
+//                     || (recipe.months[i].toLowerCase().includes("jaarrond"))) {
+//                     return recipe;
+//                 }
+//             }
+//         }).map((recipe) => (
+//             <>
+//                 <ul className="recipes__ul">
+//                     <div>
+//                         <li key={recipe.id}
+//                             className="recipes__li">
+//                             <Link
+//                                 to={"/recipe/" + recipe.id}
+//                                 className="recipes__a"
+//                             >
+//                                 {recipe.file &&
+//                                     <img
+//                                         src={recipe.file.url}
+//                                         alt={recipe.name}
+//                                         className="recipes__image"
+//                                     />}
+//                                 {recipe.title}
+//                             </Link>
+//                         </li>
+//                     </div>
+//                 </ul>
+//             </>
+//         )))
+//     }
+// </article>
+//
+//
+// {!tag.includes("selecteer") && <h3>Geselecteerd op categorie {tag}</h3>}
+// <article className="recipes__article--flex">
+//     {!tag.includes("selecteer") && (
+//         recipes.filter(recipe => {
+//             for (let i = 0; i < recipe.tags.length; i++) {
+//                 if (recipe.tags[i].toLowerCase().includes(tag.toLowerCase())) {
+//                     return recipe;
+//                 }
+//             }
+//         }).map((recipe) => (
+//             <>
+//                 <ul className="recipes__ul">
+//                     <div>
+//                         <li key={recipe.id}
+//                             className="recipes__li">
+//                             <Link
+//                                 to={"/recipe/" + recipe.id}
+//                                 className="recipes__a"
+//                             >
+//                                 {recipe.file &&
+//                                     <img
+//                                         src={recipe.file.url}
+//                                         alt={recipe.name}
+//                                         className="recipes__image"
+//                                     />}
+//                                 {recipe.title}
+//                             </Link>
+//                         </li>
+//                     </div>
+//                 </ul>
+//             </>
+//         )))
+//     }
+// </article>
--- src/pages/recipes/Recipes.js
+++ src/pages/recipes/Recipes.js
@@ -3,14 +3,56 @@ import './Recipes.css';
 import {Link} from "react-router-dom";
 import axios from "axios";
 import {AuthContext} from "../../context/AuthContext";
-import login from "../login/Login";
 
 function Recipe() {
     const {user} = useContext(AuthContext);
     const [recipes, setRecipes] = useState([]);
-    // const [months, setMonths] = useState([]);
     const [query, setQuery] = useState("");
     const token = localStorage.getItem('token');
+
+    const [month, setMonth] = useState("selecteer");
+    const [tag, setTag] = useState("selecteer");
+    const [monthsList] = useState([
+        {
+            label: "selecteer een maand",
+            value: "months"
+        },
+        {label: "januari", value: "januari"},
+        {label: "februari", value: "februari"},
+        {label: "maart", value: "maart"},
+        {label: "april", value: "april"},
+        {label: "mei", value: "mei"},
+        {label: "juni", value: "juni"},
+        {label: "juli", value: "juli"},
+        {label: "augustus", value: "augustus"},
+        {label: "september", value: "september"},
+        {label: "oktober", value: "oktober"},
+        {label: "november", value: "november"},
+        {label: "december", value: "december"}
+    ]);
+
+    const [tagsList] = useState([
+        {
+            label: "selecteer een categorie",
+            value: "tags"
+        },
+        {label: "vegetarisch", value: "vegetarisch"},
+        {label: "veganistisch", value: "veganistisch"},
+        {label: "lactosevrij", value: "lactosevrij"},
+        {label: "glutenvrij", value: "glutenvrij"},
+        {label: "ontbijt", value: "ontbijt"},
+        {label: "lunch", value: "lunch"},
+        {label: "diner", value: "diner"},
+        {label: "snack", value: "snack"},
+        {label: "bijgerecht", value: "bijgerecht"},
+        {label: "voorgerecht", value: "voorgerecht"},
+        {label: "hoofdgerecht", value: "hoofdgerecht"},
+        {label: "drinken", value: "drinken"},
+        {label: "alcoholisch", value: "alcoholisch"},
+        {label: "open vuur", value: "open vuur"},
+        {label: "dutch oven", value: "dutch oven"}
+    ]);
+
 
     // method to get an overview of all recipes
     useEffect(() => {
@@ -24,10 +66,8 @@ function Recipe() {
                 });
                 console.log("Response get all recipes:")
                 console.log(response.data);
-                console.log(response.data[0].months);
-                // console.log("Response.status: " + response.status);
+                // console.log(response.data[0].months);
                 setRecipes(response.data);
-                // setMonths(response.data);
             } catch (e) {
                 console.error(e);
             }
@@ -37,119 +77,91 @@ function Recipe() {
     }, [token])
 
 
-    //searchbar
-    const handleChange = (e) => {
-        e.preventDefault();
-        setQuery(e.target.value);
-    };
-
-
-    // if (searchInput.length > 0) {
-    //     // months.filter((month) => {
-    //     //     return month.match(searchInput);
-    //     const filtered = recipes.filter(recipe => {
-    //         return recipe.months == searchInput;
-    //     });
-    //     setFilteredRecipes(filtered);
-    //     console.log("FilteredRecipes");
-    //     console.log(filteredRecipes);
-    // }
-
-
     return (
-        <article className="page recipes-page">
-
-            <section>
+        <section className="page recipes-page">
+            <article className="recipes__article">
                 <div className="recipes__div--space-between">
-                    {/*<h2>Aantal recepten: {recipes.length}</h2>*/}
-                    {user.firstname ? <h1>Welkom {user.firstname}</h1> : <h1>Welkom</h1>}
-                    <h1>Recepten</h1>
+                    {user.firstname ? <h1>Welkom {user.firstname} bij recepten</h1> : <h1>Welkom bij recepten</h1>}
                 </div>
 
-                {/*searchbar*/}
-                {/*<label htmlFor="months">zoek op maand:*/}
-                {/*    <input*/}
-                {/*        type="text"*/}
-                {/*        placeholder="zoek op maand"*/}
-                {/*        onChange={handleChange}*/}
-                {/*        value={searchInput}*/}
-                {/*    />*/}
-                {/*    <select id="months">*/}
-                {/*        <option value="januari">januari</option>*/}
-                {/*        <option value="februari">februari</option>*/}
-                {/*        <option value="maart">maart</option>*/}
-                {/*        <option value="april">april</option>*/}
-                {/*        <option value="mei">mei</option>*/}
-                {/*        <option value="juni">juni</option>*/}
-                {/*        <option value="juli">juli</option>*/}
-                {/*        <option value="augustus">augustus</option>*/}
-                {/*        <option value="september">september</option>*/}
-                {/*        <option value="oktober">oktober</option>*/}
-                {/*        <option value="november">november</option>*/}
-                {/*        <option value="december">december</option>*/}
-                {/*    </select>*/}
-                {/*</label>*/}
+                <h3>Zoek op woord (in titel of in ingredient), maand of categorie</h3>
+                <input
+                    id="search"
+                    placeholder="zoeken..."
+                    onChange={(e) => setQuery(e.currentTarget.value)}
+                />
 
+                <select
+                    className="recipes__select"
+                    value={month}
+                    onChange={(e) => setMonth(e.currentTarget.value)}>
+                    {monthsList.map(item => (
+                        <option
+                            key={item.value}
+                            value={item.value}
+                        >
+                            {item.label}
+                        </option>
+                    ))}
+                </select>
 
-                <label htmlFor="search"> Typ hier je zoekwoord
-                    <input id="search" placeholder="zoeken..." onChange={handleChange}/>
-                </label>
+                <select
+                    className="recipes__select"
+                    value={tag}
+                    onChange={(e) => setTag(e.currentTarget.value)}>
+                    {tagsList.map(item => (
+                        <option
+                            key={item.value}
+                            value={item.value}
+                        >
+                            {item.label}
+                        </option>
+                    ))}
+                </select>
+            </article>
 
-                <h3>Boe</h3>
+            {/*tiles............................................................................................................................................*/}
 
-                {/*nog nadenken hoe de zoekfunctie eruit komt te zien en alle tiles van de recepten er dan inzetten*/}
-                {/*{*/}
-                {/*    recipes.filter(recipe => {*/}
-                {/*        if (query === "") {*/}
-                {/*            return recipe;*/}
-                {/*        } else if (recipe.title.toLowerCase().includes(query.toLowerCase())) {*/}
-                {/*            return recipe;*/}
-                {/*        }*/}
-                {/*    }).map((recipe) => (*/}
-                {/*        <p>{recipe.title}</p>*/}
-                {/*    ))*/}
-                {/*}*/}
+            <article className="recipes__article--flex">
+                {((query === "") && (month.includes("selecteer") && tag.includes("selecteer")) &&
+                    recipes.filter(recipe => {
+                        return recipe;
+                    }).map((recipe) => (
+                        <ul className="recipes__ul">
+                            <div>
+                                <li key={recipe.id}
+                                    className="recipes__li">
+                                    <Link
+                                        to={"/recipe/" + recipe.id}
+                                        className="recipes__a"
+                                    >
+                                        {recipe.file &&
+                                            <img
+                                                src={recipe.file.url}
+                                                alt={recipe.name}
+                                                className="recipes__image"
+                                            />}
+                                        {recipe.title}
+                                    </Link>
+                                </li>
+                            </div>
+                        </ul>
+                    )))
+                }
+            </article>
 
-
-                {/*{*/}
-                {/*    recipes.filter(recipe => {*/}
-                {/*        if (query === "") {*/}
-                {/*            return recipe;*/}
-                {/*        } else {*/}
-                {/*            for (let i = 0; i < recipe.months.length; i++) {*/}
-                {/*                if (recipe.months[i].toLowerCase().includes(query.toLowerCase())) {*/}
-                {/*                    return recipe;*/}
-                {/*                }*/}
-                {/*            }*/}
-                {/*        }*/}
-                {/*    }).map((recipe) => (*/}
-                {/*        <p key={recipe.id}>{recipe.title}</p>*/}
-                {/*    ))*/}
-                {/*}*/}
-
-
-                {/*{*/}
-                {/*    recipes.filter(recipe => {*/}
-                {/*        if (query === "") {*/}
-                {/*            return recipe;*/}
-                {/*        } else {*/}
-                {/*            for (let i = 0; i < recipe.tags.length; i++) {*/}
-                {/*                if (recipe.tags[i].toLowerCase().includes(query.toLowerCase())) {*/}
-                {/*                    return recipe;*/}
-                {/*                }*/}
-                {/*            }*/}
-                {/*        }*/}
-                {/*    }).map((recipe) => (*/}
-                {/*        <p key={recipe.id}>{recipe.title}</p>*/}
-                {/*    ))*/}
-                {/*}*/}
-
-
-                {
+            {query !== "" &&
+                <h3 className="recipes__h3">Geselecteerd op zoekterm "{query}"</h3>}
+            <article className="recipes__article--flex">
+                {query !== "" &&
                     recipes.filter(recipe => {
                         if (query === "") {
                             return recipe;
+                            //if query is not empty alias else
+                        } else if (recipe.title.toLowerCase().includes(query.toLowerCase())) {
+                            return recipe;
                         } else {
+                            //search for ingredients
                             for (let i = 0; i < recipe.ingredients.length; i++) {
                                 if (recipe.ingredients[i].ingredient_name.toLowerCase().includes(query.toLowerCase())) {
                                     return recipe;
@@ -157,37 +169,103 @@ function Recipe() {
                             }
                         }
                     }).map((recipe) => (
-                        <p key={recipe.id}>{recipe.title}</p>
+                        <ul className="recipes__ul">
+                            <div>
+                                <li key={recipe.id}
+                                    className="recipes__li">
+                                    <Link
+                                        to={"/recipe/" + recipe.id}
+                                        className="recipes__a"
+                                    >
+                                        {recipe.file &&
+                                            <img
+                                                src={recipe.file.url}
+                                                alt={recipe.name}
+                                                className="recipes__image"
+                                            />}
+                                        {recipe.title}
+                                    </Link>
+                                </li>
+                            </div>
+                        </ul>
                     ))
                 }
+            </article>
 
 
-                <h3>Boe</h3>
+            {!month.includes("selecteer") &&
+                <h3 className="recipes__h3">Geselecteerd op de maand {month}, inclusief jaarrond</h3>}
+            <article className="recipes__article--flex">
+                {!month.includes("selecteer") && (
+                    recipes.filter(recipe => {
+                        for (let i = 0; i < recipe.months.length; i++) {
+                            if (recipe.months[i].toLowerCase().includes(month.toLowerCase())
+                                || (recipe.months[i].toLowerCase().includes("jaarrond"))) {
+                                return recipe;
+                            }
+                        }
+                    }).map((recipe) => (
+                        <>
+                            <ul className="recipes__ul">
+                                <div>
+                                    <li key={recipe.id}
+                                        className="recipes__li">
+                                        <Link
+                                            to={"/recipe/" + recipe.id}
+                                            className="recipes__a"
+                                        >
+                                            {recipe.file &&
+                                                <img
+                                                    src={recipe.file.url}
+                                                    alt={recipe.name}
+                                                    className="recipes__image"
+                                                />}
+                                            {recipe.title}
+                                        </Link>
+                                    </li>
+                                </div>
+                            </ul>
+                        </>
+                    )))
+                }
+            </article>
 
 
-                <ul className="recipes__ul">
-                    <div className="recipes__div">
-                        {recipes.map((recipe) => {
-                            return <li key={recipe.id}
-                                       className="recipes__li">
-                                <Link
-                                    to={"/recipe/" + recipe.id}
-                                    className="recipes__a"
-                                >
-                                    {recipe.file &&
-                                        <img
-                                            src={recipe.file.url}
-                                            alt={recipe.name}
-                                            className="recipes__image"
-                                        />}
-                                    {recipe.title}
-                                </Link>
-                            </li>
-                        })}
-                    </div>
-                </ul>
-            </section>
-        </article>
+            {!tag.includes("selecteer") && <h3 className="recipes__h3">Geselecteerd op categorie {tag}</h3>}
+            <article className="recipes__article--flex">
+                {!tag.includes("selecteer") && (
+                    recipes.filter(recipe => {
+                        for (let i = 0; i < recipe.tags.length; i++) {
+                            if (recipe.tags[i].toLowerCase().includes(tag.toLowerCase())) {
+                                return recipe;
+                            }
+                        }
+                    }).map((recipe) => (
+                        <>
+                            <ul className="recipes__ul">
+                                <div>
+                                    <li key={recipe.id}
+                                        className="recipes__li">
+                                        <Link
+                                            to={"/recipe/" + recipe.id}
+                                            className="recipes__a"
+                                        >
+                                            {recipe.file &&
+                                                <img
+                                                    src={recipe.file.url}
+                                                    alt={recipe.name}
+                                                    className="recipes__image"
+                                                />}
+                                            {recipe.title}
+                                        </Link>
+                                    </li>
+                                </div>
+                            </ul>
+                        </>
+                    )))
+                }
+            </article>
+        </section>
     )
         ;
 }
